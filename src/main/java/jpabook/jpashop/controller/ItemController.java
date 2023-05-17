153858c7c56f560b41c6fe89be5cf73ba4939c98
sunwooKim <frod5@kakao.com>
2023-05-17 14:48:33 +0900

    실전 스프링부트와 JPA 활용1 - 웹계층 개발 - 변경감지와 merge(병합)

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -65,7 +65,7 @@ public class ItemController {
 
     @PostMapping("items/{itemId}/edit")
     public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
-        Book book = new Book();
+        Book book = new Book(); //임의로 만들어낸 엔티티도 기존 식별자를 가지고 있으면 준영속 엔티티로 볼 수 있다.
         book.setId(form.getId());
         book.setName(form.getName());
         book.setPrice(form.getPrice());
--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repositroy.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    //변경감지를 사용하는게 Best practice
+    @Transactional
+    public void updateItem(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId); //영속성 컨텍스트
+        //추가적으로 setter로 변경감지 하는것보다도 findItem.change()이런식으로 entity에 생성로직 만든게 유지보수에 좋다.
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
     }
 
     @Transactional(readOnly = true)
